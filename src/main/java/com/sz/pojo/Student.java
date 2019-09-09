a000a5c75cdb33752a657caf66cfac92ef74a5b1
souvenirding <dingtixin@gmail.com>
2019-09-09 19:44:27 +0800

    添加运行结果的SQL

--- src/main/java/com/sz/pojo/Student.java
+++ src/main/java/com/sz/pojo/Student.java
@@ -2,12 +2,14 @@ package com.sz.pojo;
 
 import com.baomidou.mybatisplus.annotation.SqlCondition;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
 @TableName("tblstudent")
 public class Student {
+    @TableId
     private String stuid;
     private String stuname;
     private int stuage;
--- src/test/java/com/sz/MybatisPlusDemoApplicationTests.java
+++ src/test/java/com/sz/MybatisPlusDemoApplicationTests.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -28,6 +30,8 @@ public class MybatisPlusDemoApplicationTests {
     }
 
     //条件查询:名字中有张，年龄小于40；
+//    DEBUG==>  Preparing: SELECT stuid,stusex,stuage,stuname FROM tblstudent WHERE (stuname LIKE ? AND stuage < ?)
+//    DEBUG==> Parameters: %张%(String), 40(Integer)
     @Test
     public void selectByWrapper() {
         QueryWrapper<Student> wrapper = new QueryWrapper<>();
@@ -37,6 +41,8 @@ public class MybatisPlusDemoApplicationTests {
     }
 
     //条件查询:年龄在17-19；
+//    DEBUG==>  Preparing: SELECT stuid,stusex,stuage,stuname FROM tblstudent WHERE (stuage BETWEEN ? AND ?)
+//    DEBUG==> Parameters: 17(Integer), 19(Integer)
     @Test
     public void selectByWrapper2() {
         QueryWrapper<Student> wrapper = new QueryWrapper<>();
@@ -46,6 +52,7 @@ public class MybatisPlusDemoApplicationTests {
     }
 
     //条件查询:年龄不为空；
+//    DEBUG==>  Preparing: SELECT stuid,stusex,stuage,stuname FROM tblstudent WHERE (stuage IS NOT NULL)
     @Test
     public void selectByWrapper3() {
         QueryWrapper<Student> wrapper = new QueryWrapper<>();
@@ -55,6 +62,8 @@ public class MybatisPlusDemoApplicationTests {
     }
 
     //条件查询:姓张 或者 年龄>=19，按年龄降序，年龄相同，按id升序；
+//    DEBUG==>  Preparing: SELECT stuid,stusex,stuage,stuname FROM tblstudent WHERE (stuname LIKE ? OR stuage >= ?) ORDER BY stuage DESC , stuid ASC
+//    DEBUG==> Parameters: 张%(String), 19(Integer)
     @Test
     public void selectByWrapper4() {
         QueryWrapper<Student> wrapper = new QueryWrapper<>();
@@ -65,6 +74,8 @@ public class MybatisPlusDemoApplicationTests {
     }
 
     //条件查询:姓张 或者 年龄>=19并且年龄<=21并且性别不为空；只返回第一条记录
+//    DEBUG==>  Preparing: SELECT stuid,stusex,stuage,stuname FROM tblstudent WHERE (stuname LIKE ? OR ( (stuage >= ? AND stuage <= ? AND stusex IS NOT NULL) )) limit 1
+//    DEBUG==> Parameters: 张%(String), 19(Integer), 21(Integer)
     @Test
     public void selectByWrapper5() {
         QueryWrapper<Student> wrapper = new QueryWrapper<>();
@@ -76,48 +87,67 @@ public class MybatisPlusDemoApplicationTests {
     }
 
     //查询所有，但只要id和name
+    //DEBUG==>  Preparing: SELECT stuid,stuname FROM tblstudent
     @Test
     public void select2() {
         QueryWrapper<Student> wrapper = new QueryWrapper<>();
-        wrapper.select("stuid","stuname");
+        wrapper.select("stuid", "stuname");
         List<Student> list = studentMapper.selectList(wrapper);
         list.forEach(System.out::print);
     }
 
     //查询所有，但不要sex和age
+    //DEBUG==>  Preparing: SELECT stuid,stuname FROM tblstudent
     @Test
     public void select3() {
         QueryWrapper<Student> wrapper = new QueryWrapper<>();
-        wrapper.select(Student.class,stu->!stu.getColumn().equals("stusex")&&!stu.getColumn().equals("stuage"));
+        wrapper.select(Student.class, stu -> !stu.getColumn().equals("stusex") && !stu.getColumn().equals("stuage"));
 
         List<Student> list = studentMapper.selectList(wrapper);
         list.forEach(System.out::print);
     }
 
     //condition:多用于模糊查询,like(boolean condition, R column, Object val)
+//    DEBUG==>  Preparing: SELECT stuid,stusex,stuage,stuname FROM tblstudent WHERE (stuname LIKE ?)
+//    DEBUG==> Parameters: %张%(String)
     @Test
     public void select4() {
-        String stuname="张";
-        String stusex="";
+        String stuname = "张";
+        String stusex = "";
         QueryWrapper<Student> wrapper = new QueryWrapper<>();
-        wrapper.like(StringUtils.isNotEmpty(stuname),"stuname",stuname)
-                .like(StringUtils.isNotEmpty(stusex),"stusex",stusex);
+        wrapper.like(StringUtils.isNotEmpty(stuname), "stuname", stuname)
+                .like(StringUtils.isNotEmpty(stusex), "stusex", stusex);
         List<Student> list = studentMapper.selectList(wrapper);
         list.forEach(System.out::print);
     }
+
     /**
      * 参数为实体student ---> new QueryWrapper<>(student);
-     * 可以在实体类中指定一些属性
+     * 可以在实体类定义中指定一些属性
      * 例如
+     *
      * @TableField(condition = SqlCondition.LIKE)
      * private String stuname;
      * 在sql语句会显示 stuanme like ...
      * 详情见 SqlCondition类
-     *
+     * <p>
      * %s&lt;&gt;#{%s}
      * %s 表示表中的列  &lt;&gt;表示<>,即为不等于；#{%s}表示传入的值
      * 比如：like("stuname", "张")
-     *
      */
+
+    //AllEq
+//    DEBUG==>  Preparing: SELECT stusex,stuid,stuage,stuname FROM tblstudent WHERE (stuname = ? AND stuage = ?)
+//    DEBUG==> Parameters: 张三丰(String), 18(Integer)
+    @Test
+    public void testAllEq() {
+        QueryWrapper<Student> wrapper = new QueryWrapper<>();
+        Map<String, Object> paramas = new HashMap<>();
+        paramas.put("stuname", "张三丰");
+        paramas.put("stuage", 18);
+        wrapper.allEq(paramas);
+        List<Student> list = studentMapper.selectList(wrapper);
+        list.forEach(System.out::print);
+    }
 
 }
